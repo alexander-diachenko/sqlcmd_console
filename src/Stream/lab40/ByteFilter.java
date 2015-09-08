f7139d6c2991c830d0d40a3b9041e8f47863fad6
POSITIV <mail.positiv@gmail.com>
2015-09-08 20:18:52 +0300

    паттерн команда

--- src/Stream/lab40/ByteFilter.java
+++ src/Stream/lab40/ByteFilter.java
@@ -6,15 +6,42 @@ import java.io.OutputStream;
 
 public class ByteFilter {
 
-        public static void filter (InputStream src, OutputStream dst, int bufferSize, byte filterCriteria)throws
-        IOException {
+    public static void filter(InputStream src, OutputStream dst, int bufferSize, byte filterCriteria) throws IOException {
         /*BODY*/
-            StringBuilder text = new StringBuilder();
-            byte[] buffer = new byte[bufferSize];
-            byte[] result = new byte[100];
-            while (src.read(buffer) != -1) {
-           String str = new String(buffer);
-                text.append(str);
+        // 1, 0, 0, 2, 34, 5, 6, 7, 78, 8, 9, 0, 2, 3, 5, 6, 7, 7
+
+//[read(b[4]), write(b[4],0,1), write(b[4],3,1), read(b[4]), write(b[4],0,4), read(b[4]), write(b[4],0,3), read(b[4]), write(b[4],0,4), read(b[4]), write(b[4],0,2), read(b[4])]
+
+//[read(b[4]), write(b[4],0,1), write(b[4],3,1), read(b[4]), write(b[4],0,1), write(b[4],1,1), write(b[4],2,1), write(b[4],3,1), read(b[4]), write(b[4],0,1), write(b[4],1,1), write(b[4],2,1), read(b[4]), write(b[4],0,1), write(b[4],1,1), write(b[4],2,1), write(b[4],3,1), read(b[4]), write(b[4],0,1), write(b[4],1,1), read(b[4])]
+
+
+        byte[] buffer = new byte[bufferSize];
+        int len = src.read(buffer);
+        while (len != -1) {
+            int start = 0;
+            int count = 0;
+            boolean flag = true;
+            for (int index = 0; index < len; index++) {
+                if (buffer[index] != filterCriteria) {
+                    if (flag) {
+                        start = index;
+                        flag = false;
+                    }
+                    count++;
+                    if (index < len - 1) {
+                        continue;
+                    }
+                }
+                if (count != 0) {
+                    dst.write(buffer, start, count);
+                    count = 0;
+                    flag = true;
+                }
             }
+            len = src.read(buffer);
+            dst.close();
+            src.close();
         }
     }
+}
+
