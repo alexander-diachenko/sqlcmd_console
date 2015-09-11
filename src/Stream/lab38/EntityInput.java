9aa43d8964e775822b49756e516bfce0a0c8e914
POSITIV <mail.positiv@gmail.com>
2015-09-11 07:15:52 +0300

    11

--- src/Stream/lab38/EntityInput.java
+++
@@ -1,8 +0,0 @@
-package Stream.lab38;
-
-import java.io.IOException;
-
-public interface EntityInput {
-    public Person readPerson() throws IOException;
-    public Point readPoint() throws IOException;
-}
--- src/Stream/lab39/EntityInput.java
+++
@@ -1,9 +0,0 @@
-package Stream.lab39;
-
-import java.io.*;
-
-public interface EntityInput {
-    public Person readPerson() throws IOException;
-
-    public Point readPoint() throws IOException;
-}
