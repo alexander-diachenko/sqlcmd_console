2f675c60e30c704fe747982801aadabcd9600b47
POSITIV <mail.positiv@gmail.com>
2015-09-14 13:14:36 +0300

    вынес всю логику коммандр в комманды и добавил проверку подключения

--- DatabaseManagerTest/testDatabaseManager.java
+++ DatabaseManagerTest/testDatabaseManager.java
@@ -1,18 +1,15 @@
 import org.junit.Before;
 import org.junit.Test;
-import sqlcmd.Console;
-import sqlcmd.command.Command;
-import sqlcmd.command.Find;
+import sqlcmd.view.Console;
 import sqlcmd.databasemanager.DatabaseManager;
 import sqlcmd.databasemanager.JDBCDatabaseManager;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 
-public class DatabaseManagerTest {
+public class testDatabaseManager {
 
     Console console = new Console();
     DatabaseManager manager = new JDBCDatabaseManager(console);
@@ -22,27 +19,22 @@ public class DatabaseManagerTest {
         manager.connect("sqlcmd", "postgres", "123");
         manager.clear("car");
         manager.create("car", "'1' , 'ferrari' , 'red' ,'6'");
-        manager.create("car", "'2' , 'porche' , 'black' ,'1'");
+        manager.create("car", "'2' , 'porsche' , 'black' ,'1'");
         manager.create("car", "'3' , 'bmw' , 'blue' ,'3'");
     }
 
     @Test
     public void testGetTableNames() throws Exception {
-        String[] tableNames = manager.getTableNames();
-        assertEquals("[car, user]", Arrays.toString(tableNames));
+        ArrayList<String> tableNames = manager.getTableNames();
+        assertEquals("[car, user]", tableNames.toString());
     }
 
     @Test
     public void testFindAllTableDataWithCorrectData() throws SQLException {
 
-        ArrayList<String> list = manager.getTableData("car");
-        String tableData = "";
-        for(String s : list){
-            tableData += s + ",";
-        }
-        tableData = tableData.substring(0, tableData.length() - 1);
+        ArrayList<String> tableData = manager.getTableData("car");
 
-        assertEquals("4,id,name,color,age,3,ferrari,red,6", tableData);
+        assertEquals("[4, id, name, color, age, 1, ferrari, red, 6, 2, porsche, black, 1, 3, bmw, blue, 3]", tableData.toString());
     }
 
 //    @Test
@@ -72,14 +64,9 @@ public class DatabaseManagerTest {
     @Test
     public void testFindLimitOffsetTableDataWithCorrectData() throws SQLException {
 
-        ArrayList<String> list = manager.getTableData("car LIMIT 2 OFFSET 1");
-        String tableData = "";
-        for(String s : list){
-            tableData += s + ",";
-        }
-        tableData = tableData.substring(0, tableData.length() - 1);
+        ArrayList<String> tableData = manager.getTableData("car LIMIT 2 OFFSET 1");
 
-        assertEquals("4,id,name,color,age,2,porche,black,1,3,bmw,blue,3", tableData);
+        assertEquals("[4, id, name, color, age, 2, porsche, black, 1, 3, bmw, blue, 3]", tableData.toString());
     }
 //
 //    @Test
@@ -97,12 +84,12 @@ public class DatabaseManagerTest {
 //        assertEquals("Таблицы 'qwe' не существует\n", manager.getTableData("find|qwe|1|2"));
 //    }
 //
-    @Test
-    public void testClearWithCorrectData() throws SQLException {
-
-        manager.clear("car");
-        assertEquals("4", manager.getTableData("find|car"));
-    }
+//    @Test
+//    public void testClearWithCorrectData() throws SQLException {
+//
+//        manager.clear("car");
+//        assertEquals("4", manager.getTableData("find|car"));
+//    }
 //
 //    @Test
 //    public void testClearWithIncorrectData1() {
@@ -126,17 +113,15 @@ public class DatabaseManagerTest {
 //                "+---------------------------------------+\n", manager.getTableData("find|car"));
 //    }
 //
-//    @Test
-//    public void testCreateWithCorrectData() {
-//
-//        manager.clear("clear|car");
-//        manager.create("create|car|2|ferrari|red|6");
-//        assertEquals("+---------------------------------------+\n" +
-//                "| id      | name    | color   | age     |\n" +
-//                "+---------------------------------------+\n" +
-//                "| 2       | ferrari | red     | 6       |\n" +
-//                "+---------------------------------------+\n", manager.getTableData("find|car"));
-//    }
+    @Test
+    public void testCreateWithCorrectData() throws SQLException {
+
+        manager.clear("car");
+
+        ArrayList<String> tableData = manager.getTableData("car");
+
+        assertEquals("[4, id, name, color, age]", tableData.toString());
+    }
 //
 //    @Test
 //    public void testCreateWithIncorrectData() {
@@ -172,16 +157,18 @@ public class DatabaseManagerTest {
 //    }
 //
 //
-//    @Test
-//    public void testUpdateWithCorrectData() {
-//
-//        manager.update("uodate|car|id|2|name|lamborghini|color|yellow|age|3");
-//        assertEquals("+-------------------------------------------------------+\n" +
-//                "| id          | name        | color       | age         |\n" +
-//                "+-------------------------------------------------------+\n" +
-//                "| 2           | lamborghini | yellow      | 3           |\n" +
-//                "+-------------------------------------------------------+\n", manager.getTableData("find|car"));
-//    }
+    @Test
+    public void testUpdateWithCorrectData() throws SQLException {
+
+        manager.update("car", "name ='mercedes'", "id =3");
+        manager.update("car", "color ='white'", "id =3");
+        manager.update("car", "age ='10'", "id =3");
+
+        ArrayList<String> tableData = manager.getTableData("car");
+
+        assertEquals("[4, id, name, color, age, 1, ferrari, red, 6," +
+                    " 2, porsche, black, 1, 3, mercedes, white, 10]", tableData.toString());
+    }
 //
 //    @Test
 //    public void testUpdateWithIncorrectData() {
@@ -205,15 +192,15 @@ public class DatabaseManagerTest {
 //                "+---------------------------------------+\n", manager.getTableData("find|car"));
 //    }
 //
-//    @Test
-//    public void testDeleteWithCorrectData() {
-//
-//        manager.delete("delete|car|id|2");
-//        assertEquals("+-------------------------------+\n" +
-//                "| id    | name  | color | age   |\n" +
-//                "+-------------------------------+\n" +
-//                "+-------------------------------+\n", manager.getTableData("find|car"));
-//    }
+    @Test
+    public void testDeleteWithCorrectData() throws SQLException {
+
+        manager.delete("car", "id =3");
+
+        ArrayList<String> tableData = manager.getTableData("car");
+
+        assertEquals("[4, id, name, color, age, 1, ferrari, red, 6, 2, porsche, black, 1]", tableData.toString());
+    }
 //
 //    @Test
 //    public void testDeleteWithIncorrectData() {
